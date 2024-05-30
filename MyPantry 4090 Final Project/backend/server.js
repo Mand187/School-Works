fc38694f5eccfec992f48fe7206f8ecedb680471
Matthew Anderson <flyer4840@gmail.com>
2024-05-29 23:53:12 -0400

    reworking backend with examples

--- MyPantry 4090 Final Project/backend/server.js
+++ MyPantry 4090 Final Project/backend/server.js
@@ -1,22 +1,15 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-const app = express();
-
-app.use(express.json());
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
-app.use('/api/users', require('./routes/users'));
-// app.use('/api/food', require('./routes/food'));
-// app.use('/api/recipes', require('./routes/recipes'));
+import express from "express";
+import cors from "cors";
+import records from "./routes/record.js";
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
